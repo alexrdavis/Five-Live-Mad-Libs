1626d89d86ef372cfe89dc053352ab660824a7b7
Alex <alexandradavis13@gmail.com>
2022-05-29 12:15:18 -0700

    Getting inputs
    Added getName(), getNoun(), and getAdjective() for user inputs.

--- madlibs.js
+++ madlibs.js
@@ -3,4 +3,20 @@ Madlibs Project:
 Make a program that will take in user input from the consumer and then print out a Mad Lib to the console
 */
 
+// Get name from user input
+function getName() {
+    const name = prompt("Person's name ")
+    return name
+}
 
+// Get a noun from user input
+function getNoun() {
+    const noun = prompt("Enter a noun ")
+    return noun
+}
+
+// Get an adjective from user input
+function getAdjective() {
+    const adjective = prompt("Enter an adjective (feeling) ")
+    return adjective
+}
